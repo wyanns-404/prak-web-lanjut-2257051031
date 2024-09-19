fb21c20e366804d943d64bd1306f384bcc477bf7
Wayan Santie Arif <wayansantiearif@gmail.com>
2024-09-20 04:49:07 +0700

    Profile Page

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,25 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Profile</title>
+
+    @vite('resources/css/app.css')
 </head>
-<body>
-    
-    <tr>
-        <td>Nama</td>
-        <td>:</td>
-        <td><?= $nama ?></td>
-    </tr>
-    <tr>
-        <td>Kelas</td>
-        <td>:</td>
-        <td><?= $kelas ?></td>
-    </tr>
-    <tr>
-        <td>NPM</td>
-        <td>:</td>
-        <td><?= $npm ?></td>
-    </tr>
+<body style="margin: 0; padding: 0;">
+
+    <div class="container w-full h-screen flex justify-center items-center bg-gradient-to-r from-sky-500 to-indigo-500">
+        <div class="profile w-96 h-auto flex justify-center items-center flex-col bg-white/30 rounded-xl p-10">
+            <div class="img w-40">
+                <img class="rounded-full w-full shadow-xl border-4 border-blue-100" src="{{ asset('img/profile.jpg') }}" alt="profile">
+            </div>
+            <div class="biodata text-center text-blue-100">
+                <div class="biodata-data font-bold w-40 m-4 p-2 bg-blue-950/30 rounded-lg">
+                    <?= $nama ?>
+                </div>
+                <div class="biodata-data font-bold w-40 m-4 p-2 bg-blue-950/30 rounded-lg">
+                    <?= $kelas ?>
+                </div>
+                <div class="biodata-data font-bold w-40 m-4 p-2 bg-blue-950/30 rounded-lg">
+                    <?= $npm ?>
+                </div>
+            </div>
+        </div>
+    </div>
     
 </body>
 </html>
