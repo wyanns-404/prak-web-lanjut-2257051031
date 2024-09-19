c0b8f2ec99ee0e50869208f52ee5c2187586122c
Wayan Santie Arif <wayansantiearif@gmail.com>
2024-09-20 03:18:08 +0700

    Add Profile Page

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <tr>
+        <td>Nama</td>
+        <td>:</td>
+        <td><?= $nama ?></td>
+    </tr>
+    <tr>
+        <td>Kelas</td>
+        <td>:</td>
+        <td><?= $kelas ?></td>
+    </tr>
+    <tr>
+        <td>NPM</td>
+        <td>:</td>
+        <td><?= $npm ?></td>
+    </tr>
+    
+</body>
+</html>
