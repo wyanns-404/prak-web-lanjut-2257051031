b8236c6489233996e8dcdd46727a72476595837b
Wayan Santie Arif <wayansantiearif@gmail.com>
2024-09-24 13:14:49 +0700

    Update UI

--- resources/views/create_user.blade.php
+++ resources/views/create_user.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Create User</title>
+
+    @vite('resources/css/app.css')
 </head>
-<body>
-    <form action="{{ route('user.store') }}" method="POST">
+{{-- 2351031013 --}}
+<body class="m-0 p-0">
+    {{-- <form action="{{ route('user.store') }}" method="POST">
         @csrf
         <div>
             <label for="nama">Nama:</label>
@@ -25,6 +28,29 @@
         </div>
 
         <button type="submit">Submit</button>
-    </form>
+    </form> --}}
+
+    <div class="container flex justify-center items-center h-screen bg-gradient-to-r from-blue-950 to-blue-900">
+
+        <div class="bg-black/30 p-10 w-96 rounded-xl">
+            <form action="{{ route('user.store') }}" method="POST" class="max-w-sm mx-auto">
+                @csrf
+                <div class="mb-5">
+                    <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+                    <input type="text" name="nama" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                </div>
+                <div class="mb-5">
+                    <label for="kelas" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Kelas</label>
+                    <input type="text" name="kelas" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                </div>
+                <div class="mb-5">
+                    <label for="npm" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NPM</label>
+                    <input type="text" name="npm" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                </div>
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            </form>
+        </div>
+
+    </div>
 </body>
 </html>
